50b09af378ba7ef7f289326659663d2abf7abe2a
didchrist <christopher.pelette@gmail.com~>
2023-02-08 16:31:48 +0100

    Mise en place de la page disponibilité

--- src/Controller/AvailabilityController.php
+++ src/Controller/AvailabilityController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Availability;
+use App\Form\AvailabilityType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_USER')]
 class AvailabilityController extends AbstractController
 {
     #[Route('/availability', name: 'app_availability')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $availability = new Availability();
+        $form = $this->createForm(AvailabilityType::class, $availability);
+        $form->handleRequest($request);
         return $this->render('availability/index.html.twig', [
-            'controller_name' => 'AvailabilityController',
+            'availabilityForm' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/AvailabilityType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Availability;
+use App\Entity\Game;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvailabilityType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('startDate')
+            ->add('endDate')
+            ->add('id_user', EntityType::class, [
+                'class' => User::class
+            ])
+            ->add('game', EntityType::class, [
+                'class' => Game::class
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Availability::class,
+        ]);
+    }
+}
