0482dd45ea0c3f21f61e5f99b0f8277841f7bbdd
Christopher Pelette <christopher.pelette@gmail.com>
2023-02-10 11:28:23 +0100

    Fin de la première partie back sur Game

--- src/Entity/Game.php
+++ src/Entity/Game.php
@@ -11,7 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
 class Game
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy:"IDENTITY")]
+    #[ORM\GeneratedValue(strategy: "IDENTITY")]
     #[ORM\Column]
     private ?int $id = null;
 
@@ -116,5 +116,9 @@ class Game
         }
 
         return $this;
+    }
+    public function __toString()
+    {
+        return $this->getGameName();
     }
 }
