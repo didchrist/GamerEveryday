bf77deab0689addf068cf185fd8606b6a98cf8b8
Christopher Pelette <christopher.pelette@gmail.com>
2023-02-16 17:26:47 +0100

    Mise en place module ajax add, delete

--- src/Controller/AvailabilityController.php
+++ src/Controller/AvailabilityController.php
@@ -8,6 +8,7 @@ use App\Form\AvailabilityType;
 use App\Repository\AvailabilityGlobalRepository;
 use App\Repository\AvailabilityRepository;
 use App\Repository\GameRepository;
+use DateTime;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -34,9 +35,9 @@ class AvailabilityController extends AbstractController
         ]);
     }
     #[Route('/availability/listgame', name: 'app_availability_listgame')]
-    public function ajaxAvailabilitiesGame (AvailabilityRepository $availabilityRepository, Request $request) : JsonResponse
+    public function ajaxAvailabilitiesGame(AvailabilityRepository $availabilityRepository, Request $request): JsonResponse
     {
-        $game = $request->getContent();      
+        $game = $request->getContent();
 
         $user = $this->getUser();
         $availabilities = $availabilityRepository->findBy(['id_user' => $user->getId(), 'game' => $game]);
@@ -47,14 +48,14 @@ class AvailabilityController extends AbstractController
             $array[] = ['startDate' => $startDate, 'endDate' => $endDate];
         }
         if (isset($array)) {
-            Return new JsonResponse($array); 
+            return new JsonResponse($array);
         } else {
             $array = [];
-            Return new JsonResponse($array);
+            return new JsonResponse($array);
         }
     }
     #[Route('/availability/add/global', name: 'app_add_globalAvailability')]
-    public function ajaxAddGlobalAvailability (Request $request, AvailabilityGlobalRepository $availabilityGlobalRepository) : Response
+    public function ajaxAddGlobalAvailability(Request $request, AvailabilityGlobalRepository $availabilityGlobalRepository): Response
     {
         $availability = new AvailabilityGlobal();
         $user = $this->getUser();
@@ -66,26 +67,26 @@ class AvailabilityController extends AbstractController
         $endDate->setTimezone(new \DateTimeZone('UTC'));
         $availabilities = $availabilityGlobalRepository->findBy(['user_id' => $user->getId()]);
         foreach ($availabilities as $avail) {
-            if ($avail->getStartDate()<= $startDate and $startDate < $avail->getEndDate()) {
-                return new Response($startDate->format('H:i d/m/Y'). ' a déjà était inscrit en tant que disponibilité.');
+            if ($avail->getStartDate() <= $startDate and $startDate < $avail->getEndDate()) {
+                return new Response($startDate->format('H:i d/m/Y') . ' a déjà était inscrit en tant que disponibilité.');
             }
             if ($avail->getStartDate() < $endDate and $endDate <= $avail->getEndDate()) {
-                return new Response($endDate->format('H:i d/m/Y'). ' a déjà était inscrit en tant que disponibilité.');
+                return new Response($endDate->format('H:i d/m/Y') . ' a déjà était inscrit en tant que disponibilité.');
             }
         }
         $availability->setStartDate($startDate)
-                    -> setEndDate($endDate)
-                    ->setUserId($user);
-        
+            ->setEndDate($endDate)
+            ->setUserId($user);
+
         $availabilityGlobalRepository->save($availability, true);
-        
+
         return new Response('ajout effectué');
     }
     #[Route('/availability/add', name: 'app_add_availability')]
-    public function ajaxAddAvailability (Request $request, AvailabilityRepository $availabilityRepository, GameRepository $gameRepository) :Response
+    public function ajaxAddAvailability(Request $request, AvailabilityRepository $availabilityRepository, GameRepository $gameRepository): Response
     {
         $availability = new Availability();
-        $user = $this ->getUser();
+        $user = $this->getUser();
         $data = json_decode($request->getContent(), true);
 
         $startDate = new \DateTime($data['startDate'], new \DateTimeZone('Europe/Paris'));
@@ -93,23 +94,59 @@ class AvailabilityController extends AbstractController
         $endDate = new \DateTime($data['endDate'], new \DateTimeZone('Europe/Paris'));
         $endDate->setTimezone(new \DateTimeZone('UTC'));
         $game = $data['game'];
-        $availabilities = $availabilityRepository->findBy(['id_user' => $user->getId(),'game' => $game]);
+        $availabilities = $availabilityRepository->findBy(['id_user' => $user->getId(), 'game' => $game]);
         foreach ($availabilities as $avail) {
-            if ($avail->getStartDate()>= $startDate and $startDate < $avail->getEndDate()) {
-                return new Response($startDate->format('H:i d/m/Y'). ' a déjà était inscrit en tant que disponibilité.');
+            if ($avail->getStartDate() <= $startDate and $startDate < $avail->getEndDate()) {
+                return new Response($startDate->format('H:i d/m/Y') . ' a déjà était inscrit en tant que disponibilité.');
             }
-            if ($avail->getStartDate() > $endDate and $endDate <= $avail->getEndDate()) {
-                return new Response($endDate->format('H:i d/m/Y'). ' a déjà était inscrit en tant que disponibilité.');
+            if ($avail->getStartDate() < $endDate and $endDate <= $avail->getEndDate()) {
+                return new Response($endDate->format('H:i d/m/Y') . ' a déjà était inscrit en tant que disponibilité.');
             }
         }
         $game = $gameRepository->findOneBy(['id' => $game]);
         $availability->setStartDate($startDate)
-                    ->setEndDate($endDate)
-                    ->setIdUser($user)
-                    ->setGame($game);
+            ->setEndDate($endDate)
+            ->setIdUser($user)
+            ->setGame($game);
 
         $availabilityRepository->save($availability, true);
 
         return new Response('ajout effectué');
+    }
+    #[Route('/availability/delete/global', name: 'app_delete_globalAvailability')]
+    public function ajaxDeleteAvailabilityGlobal(Request $request, AvailabilityGlobalRepository $availabilityGlobalRepository): Response
+    {
+        $user = $this->getUser();
+        $data = json_decode($request->getContent(), true);
+
+        $date = new \DateTime();
+        $startDate = $date->createFromFormat('H:i d/m/Y', $data['startDate'], new \DateTimeZone('Europe/Paris'));
+        $startDate->setTimezone(new \DateTimeZone('UTC'));
+        $endDate = $date->createFromFormat('H:i d/m/Y', $data['endDate'], new \DateTimeZone('Europe/Paris'));
+        $endDate->setTimezone(new \DateTimeZone('UTC'));
+
+
+        $availability = $availabilityGlobalRepository->findOneBy(['start_date' => $startDate, 'end_date' => $endDate, 'user_id' => $user->getId()]);
+        $availabilityGlobalRepository->remove($availability, true);
+
+        return new Response('suppression effectué');
+    }
+    #[Route('/availability/delete', name: 'app_delete_availability')]
+    public function ajaxDeleteAvailability(Request $request, AvailabilityRepository $availabilityRepository, GameRepository $gameRepository): Response
+    {
+        $user = $this->getUser();
+        $data = json_decode($request->getContent(), true);
+        $game = $data['game'];
+
+        $date = new \DateTime();
+        $startDate = $date->createFromFormat('H:i d/m/Y', $data['startDate'], new \DateTimeZone('Europe/Paris'));
+        $startDate->setTimezone(new \DateTimeZone('UTC'));
+        $endDate = $date->createFromFormat('H:i d/m/Y', $data['endDate'], new \DateTimeZone('Europe/Paris'));
+        $endDate->setTimezone(new \DateTimeZone('UTC'));
+
+        $availability = $availabilityRepository->findOneBy(['start_date' => $startDate, 'end_date' => $endDate, 'user_id' => $user->getId(), 'game' => $game]);
+        $availabilityRepository->remove($availability, true);
+
+        return new Response('suppression effectué');
     }
 }
