b9cdd89d7b42c015b08b1212fd83ebcead8f2cf5
Christopher Pelette <christopher.pelette@gmail.com>
2023-03-09 16:32:54 +0100

    Fin du developpement de l'interface js dans game

--- public/assets/js/ajaxGameUser.js
+++ public/assets/js/ajaxGameUser.js
@@ -1,86 +1,81 @@
 var formAjax = document.querySelectorAll(".form-game");
 
 function creationSwitch(element) {
-    var newlabel = document.createElement('label');
-    newlabel.setAttribute('class','switch');
-    var newChildrenInput = document.createElement('input');
-    newChildrenInput.setAttribute('type','checkbox');
-    newChildrenInput.setAttribute("checked", true);
-    newChildrenInput.setAttribute("class", "eventCheckbox");
-    var newChildrenSpan = document.createElement('span');
-    newChildrenSpan.setAttribute('class', 'slider round');
-    element.insertAdjacentElement('afterend', newlabel);
-    newlabel.append(newChildrenInput);
-    newlabel.append(newChildrenSpan);
-    newChildrenInput.addEventListener('click', function(){
-        var idgame = element.parentElement.children[0].value;
-        if (this.checked == true) {
-            data = {"idGame": idgame, "showGame": true};
-        } else {
-            data = {"idGame": idgame, "showGame": false};
-        }
-        dataJson = JSON.stringify(data);
-        //Attention problème performance possible
-        fetch(urlUpdate, { method:'post', headers: { 'Content-Type': 'application/json'}, body:dataJson})
-            .then(() => {
-
-            })
-            .catch(function(error) {
-                console.log('Erreur dans l\'opération fetch :' + error.message);
-            })
-    });
-    
+  var newlabel = document.createElement("label");
+  newlabel.setAttribute("class", "switch");
+  var newChildrenInput = document.createElement("input");
+  newChildrenInput.setAttribute("type", "checkbox");
+  newChildrenInput.setAttribute("checked", true);
+  newChildrenInput.setAttribute("class", "eventCheckbox");
+  var newChildrenSpan = document.createElement("span");
+  newChildrenSpan.setAttribute("class", "slider round");
+  element.insertAdjacentElement("afterend", newlabel);
+  newlabel.append(newChildrenInput);
+  newlabel.append(newChildrenSpan);
+  newChildrenInput.addEventListener("click", function () {
+    var idgame = element.parentElement.children[0].value;
+    if (this.checked == true) {
+      data = { idGame: idgame, showGame: true };
+    } else {
+      data = { idGame: idgame, showGame: false };
+    }
+    dataJson = JSON.stringify(data);
+    //Attention problème performance possible
+    fetch(urlUpdate, { method: "post", headers: { "Content-Type": "application/json" }, body: dataJson })
+      .then(() => {})
+      .catch(function (error) {
+        console.log("Erreur dans l'opération fetch :" + error.message);
+      });
+  });
 }
-formAjax.forEach(function(element) {
-        element.addEventListener('submit', function (event) {
-        event.preventDefault();
-        form = new FormData(this);
-        var hasGameUser = this.children[1].textContent;
-        if (hasGameUser == 'Je possède ce jeu') {
-            fetch(urlAdd, { method: 'post', body:form})
-                .then(() => {
-                    var input =this.children[1];
-                    creationSwitch(input);
-                    input.className = "fond-neon-possede";
-                    input.innerHTML = "Posséder";
-                })
-                .catch(function(error) {
-                    console.log('Erreur dans l\'opération fetch :' + error.message);
-                })
-        } else {
-            fetch(urlDelete, { method: 'post', body:form})
-                .then(() => {
-                    var input =this.children[1];
-                    var checkbox = this.children[2];
-                    checkbox.remove();
-                    input.className = "fond-neon-non-possede";
-                    input.innerHTML = "Non-posséder";
-                }) 
-                .catch(function(error) {
-                    console.log('Erreur dans l\'opération fetch :' + error.message);
-                })
-        }
-    })
-})
+formAjax.forEach(function (element) {
+  element.addEventListener("submit", function (event) {
+    event.preventDefault();
+    form = new FormData(this);
+    var hasGameUser = this.children[1].textContent;
+    if (hasGameUser == "Posséder") {
+      fetch(urlAdd, { method: "post", body: form })
+        .then(() => {
+          var input = this.children[1];
+          creationSwitch(input);
+          input.className = "fond-neon-possede";
+          input.innerHTML = "Non-posséder";
+        })
+        .catch(function (error) {
+          console.log("Erreur dans l'opération fetch :" + error.message);
+        });
+    } else {
+      fetch(urlDelete, { method: "post", body: form })
+        .then(() => {
+          var input = this.children[1];
+          var checkbox = this.children[2];
+          checkbox.remove();
+          input.className = "fond-neon-non-possede";
+          input.innerHTML = "Posséder";
+        })
+        .catch(function (error) {
+          console.log("Erreur dans l'opération fetch :" + error.message);
+        });
+    }
+  });
+});
 
-var checkbox = document.querySelectorAll('.eventCheckbox');
+var checkbox = document.querySelectorAll(".eventCheckbox");
 
-checkbox.forEach(function(element) {
-    element.addEventListener('click', function AjaxUpdate() {
-        var idgame = this.parentElement.parentElement.children[0].value;
-        if (this.checked == true) {
-            data = {"idGame": idgame, "showGame": true};
-        } else {
-            data = {"idGame": idgame, "showGame": false};
-        }
-        dataJson = JSON.stringify(data);
-        //Attention problème performance possible
-        fetch(urlUpdate, { method:'post', headers: { 'Content-Type': 'application/json'}, body:dataJson})
-            .then(() => {
-
-            })
-            .catch(function(error) {
-                console.log('Erreur dans l\'opération fetch :' + error.message);
-            })
-    })
-})
+checkbox.forEach(function (element) {
+  element.addEventListener("click", function AjaxUpdate() {
+    var idgame = this.parentElement.parentElement.children[0].value;
+    if (this.checked == true) {
+      data = { idGame: idgame, showGame: true };
+    } else {
+      data = { idGame: idgame, showGame: false };
+    }
+    dataJson = JSON.stringify(data);
+    //Attention problème performance possible
+    fetch(urlUpdate, { method: "post", headers: { "Content-Type": "application/json" }, body: dataJson })
+      .then(() => {})
+      .catch(function (error) {
+        console.log("Erreur dans l'opération fetch :" + error.message);
+      });
+  });
+});
--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -18,33 +18,32 @@ class GameController extends AbstractController
     public function index(GameRepository $gameRepository): Response
     {
         $user = $this->getUser();
-    
+
         $games = $gameRepository->findAllGameByUser($user->getId());
         return $this->render('game/index.html.twig', [
             'games' => $games
         ]);
     }
     #[Route('/game/user/add', name: 'app_add_gameUser')]
-    public function ajaxAddGameUser (GameUserRepository $gameUserRepository, GameRepository $gameRepository, Request $request) :Response
+    public function ajaxAddGameUser(GameUserRepository $gameUserRepository, GameRepository $gameRepository, Request $request): Response
     {
         $gameUser = new GameUser();
 
         $user = $this->getUser();
 
         $game = $request->get('gameId');
-        $game = $gameRepository->findOneBy(['id'=> $game]);
+        $game = $gameRepository->findOneBy(['id' => $game]);
 
         $gameUser->setIdGame($game);
         $gameUser->setShowGame(true);
         $gameUser->setIdUser($user);
 
-
         $gameUserRepository->save($gameUser, true);
 
         return new Response('Modification faite');
     }
-    #[Route('/game/user/delete', name:'app_delete_gameUser')]
-    public function ajaxRemoveGameUser (GameUserRepository $gameUserRepository, Request $request) : Response
+    #[Route('/game/user/delete', name: 'app_delete_gameUser')]
+    public function ajaxRemoveGameUser(GameUserRepository $gameUserRepository, Request $request): Response
     {
         $user = $this->getUser();
         $game = $request->get('gameId');
@@ -55,13 +54,13 @@ class GameController extends AbstractController
 
         return new Response('Suppression faite');
     }
-    #[Route('/game/user/modify', name:'app_modify_gameUser')]
-    public function ajaxUpdateGameUser (GameUserRepository $gameUserRepository, Request $request) : Response
+    #[Route('/game/user/modify', name: 'app_modify_gameUser')]
+    public function ajaxUpdateGameUser(GameUserRepository $gameUserRepository, Request $request): Response
     {
         //Attention problème performance possible
         $user = $this->getUser();
         $data = json_decode($request->getContent(), true);
-        
+
         $game = $data['idGame'];
         $showGame = $data['showGame'];
 
