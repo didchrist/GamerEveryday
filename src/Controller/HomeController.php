c554d71196d46c8dee8ef6b7b238fc5bef88a3b9
Ali <93247826+bakcloud@users.noreply.github.com>
2023-03-10 21:43:41 +0100

    push du portfolio en ligne (non fonctionnel en local) modif du css table width 100% -> 50% (temporaire)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,19 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        $server=$_SERVER["SERVER_NAME"];
+        switch($server){
+            case 'www.ali.stage-cda.fr':
+            $html='home/ali.html.twig';
+                break;
+            case 'www.christopher.stage-cda.fr':
+            $html='home/christopher.html.twig';
+                break;
+            default:
+            $html='home/index.html.twig';
+                break;
+        }
+        return $this->render("$html", [
             'controller_name' => 'HomeController',
         ]);
     }
